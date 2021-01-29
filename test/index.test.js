b9e61d05c3f5eba561212837ab94f5b27f876916
Martin <m@xnx.no>
2021-01-29 08:00:05 +0100

    Initial

--- test/index.test.js
+++
@@ -1,5 +0,0 @@
-describe("should test", () => {
-    it("should test", async () => {
-        expect(require("../dist")).toBeDefined();
-    });
-});
---
+++ tests/index.test.js
@@ -0,0 +1,5 @@
+describe("should tests", () => {
+    it("should tests", async () => {
+        expect(require("../dist")).toBeDefined();
+    });
+});
